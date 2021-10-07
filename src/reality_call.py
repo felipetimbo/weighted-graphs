6fe2751e80e2336b84220e9ffc5612dd9f422ecb
Felipe Timbo <timbo.felipe@gmail.com>
2021-10-06 21:56:38 -0300

    adding 'reality-call' dataset

--- src/reality_call.py
+++ src/reality_call.py
@@ -2,12 +2,11 @@ import os
 import pandas as pd
 import numpy as np
 
-def preprocess_copenhagen():
-    calls_df = pd.read_csv(os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..', 'unweighted-graphs','copenhagen-interaction','calls.csv')), header=0, names=['timestamp', 'orig','dest','duration'])
-    sms_df = pd.read_csv(os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..', 'unweighted-graphs','copenhagen-interaction','sms.csv')), header=0, names=['timestamp', 'orig','dest'])
+def preprocess_reality_call():
+    df = pd.read_csv(os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..', 'unweighted-graphs','reality-call','ia-reality-call.csv')), header=0, names=['orig','dest','timestamp','duration'])
 
-    df = pd.concat([calls_df[['orig', 'dest']], sms_df[['orig','dest']]]).reset_index(drop=True)
-    
+    df = df[df['orig'] != df['dest']].reset_index(drop=True)
+
     # adding two new columns to get min and max origin and destination users
     df['user1'] = df[['orig','dest']].min(axis=1)
     df['user2'] = df[['orig','dest']].max(axis=1)
@@ -24,8 +23,8 @@ def preprocess_copenhagen():
     # weight is the sum of calls and messages among two users 
     df = df.groupby(['user1', 'user2']).size().reset_index(name='weight')
 
-    df.to_csv(os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..', 'weighted-graphs','copenhagen-interaction.csv')), index=False, header=False)
+    df.to_csv(os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..', 'weighted-graphs','reality-call.csv')), index=False, header=False)
 
 if __name__ == "__main__":
-    preprocess_copenhagen()
+    preprocess_reality_call()
     
