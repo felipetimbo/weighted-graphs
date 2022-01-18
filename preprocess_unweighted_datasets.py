d3785b7c43280f140916074881a853fe4740867c
Felipe Timbo <timbo.felipe@gmail.com>
2022-01-18 19:04:58 -0300

    adding DBLP graph preprocessing

--- preprocess_unweighted_datasets.py
+++ preprocess_unweighted_datasets.py
@@ -148,13 +148,63 @@ def preprocess_stackoverflow():
 
     df.to_csv(os.path.abspath(os.path.join(os.path.dirname( __file__ ), 'weighted-graphs','sx-stackoverflow.csv')), index=False, header=False)
 
+def preprocess_enron():
+    df = pd.read_csv(os.path.abspath(os.path.join(os.path.dirname( __file__ ), 'unweighted-graphs','enron','enron.csv')), delimiter=' ', header=0, names=['orig','dest','timestamp'])
+
+    df = df[df['orig'] != df['dest']].reset_index(drop=True)
+
+    # adding two new columns to get min and max origin and destination users
+    df['user1'] = df[['orig','dest']].min(axis=1)
+    df['user2'] = df[['orig','dest']].max(axis=1)
+
+    # sorting and concatenating unique 'orig' and 'dest' ids  
+    ids = np.sort(np.unique(np.hstack((df['orig'].to_numpy(),df['dest'].to_numpy()))))
+    dict_ids = dict( zip( ids, list(range(len(ids)))))
+
+    # sorting and reordering ids for node1 and node2   
+    df = df.sort_values(by=['user1', 'user2'])
+    df = df.replace({'user1': dict_ids}) 
+    df = df.replace({'user2': dict_ids}) 
+
+    # weight is the sum of calls and messages among two users 
+    df = df.groupby(['user1', 'user2']).size().reset_index(name='weight')
+
+    df.to_csv(os.path.abspath(os.path.join(os.path.dirname( __file__ ), 'weighted-graphs','enron.csv')), index=False, header=False)
+
+
+def preprocess_dblp():
+
+    df = pd.read_csv(os.path.abspath(os.path.join(os.path.dirname( __file__ ), 'unweighted-graphs','dblp','edges.csv')), delimiter=';', header=0, names=['orig','dest','weight'])
+
+    df = df[df['orig'] != df['dest']].reset_index(drop=True)
+
+    # adding two new columns to get min and max origin and destination users
+    df['user1'] = df[['orig','dest']].min(axis=1)
+    df['user2'] = df[['orig','dest']].max(axis=1)
+
+    # sorting and concatenating unique 'orig' and 'dest' ids  
+    ids = np.sort(np.unique(np.hstack((df['orig'].to_numpy(),df['dest'].to_numpy()))))
+    dict_ids = dict( zip( ids, list(range(len(ids)))))
+
+    # sorting and reordering ids for node1 and node2   
+    df = df.sort_values(by=['user1', 'user2'])
+    df = df.replace({'user1': dict_ids}) 
+    df = df.replace({'user2': dict_ids}) 
+
+    # weight is the sum of calls and messages among two users 
+    df = df.groupby(['user1', 'user2']).size().reset_index(name='weight')
+
+    df.to_csv(os.path.abspath(os.path.join(os.path.dirname( __file__ ), 'weighted-graphs','dblp.csv')), index=False, header=False)
+
 
 if __name__ == "__main__":
-    preprocess_copenhagen()
-    preprocess_reality_call()
-    preprocess_contacts_dublin()
-    preprocess_digg_reply()
-    preprocess_high_school_contacts()
-    preprocess_wikitalk()
-    preprocess_stackoverflow()
+    # preprocess_copenhagen()
+    # preprocess_reality_call()
+    # preprocess_contacts_dublin()
+    # preprocess_digg_reply()
+    # preprocess_high_school_contacts()
+    # preprocess_wikitalk()
+    # preprocess_stackoverflow()
+    # preprocess_enron()
+    preprocess_dblp()
     
