045e6a7335383c69be2203f933a29bd6b278f975
Felipe Timbo <timbo.felipe@gmail.com>
2022-04-06 13:10:30 -0300

    adding new version of reality-call network

--- preprocess_unweighted_datasets.py
+++ preprocess_unweighted_datasets.py
@@ -49,6 +49,30 @@ def preprocess_reality_call():
 
     df.to_csv(os.path.abspath(os.path.join(os.path.dirname( __file__ ), 'weighted-graphs','reality-call.csv')), index=False, header=False)
 
+def preprocess_reality_call_v2():
+    df = pd.read_csv(os.path.abspath(os.path.join(os.path.dirname( __file__ ), 'unweighted-graphs','reality-call','reality-call-v2.csv')), header=0, sep=" ", names=['orig','dest','timestamp'])
+
+    df = df[df['orig'] != df['dest']].reset_index(drop=True)
+
+    # adding two new columns to get min and max origin and destination users
+    df['user1'] = df[['orig','dest']].min(axis=1)
+    df['user2'] = df[['orig','dest']].max(axis=1)
+
+    # sorting and concatenating unique 'orig' and 'dest' ids  
+    ids = np.sort(np.unique(np.hstack((df['orig'].to_numpy(),df['dest'].to_numpy()))))
+    dict_ids = dict( zip( ids, list(range(len(ids)))))
+
+    # sorting and reordering ids for node1 and node2   
+    df = df.sort_values(by=['user1', 'user2'])
+    df = df.replace({'user1': dict_ids}) 
+    df = df.replace({'user2': dict_ids}) 
+
+    # weight is the sum of calls and messages among two users 
+    df = df.groupby(['user1', 'user2']).size().reset_index(name='weight')
+
+    df.to_csv(os.path.abspath(os.path.join(os.path.dirname( __file__ ), 'weighted-graphs','reality-call2.csv')), index=False, header=False)
+
+
 def preprocess_contacts_dublin():
     df = pd.read_csv(os.path.abspath(os.path.join(os.path.dirname( __file__ ), 'unweighted-graphs','contacts-dublin','ia-contacts-dublin.csv')), header=0, names=['orig','dest','timestamp'])
 
@@ -200,11 +224,12 @@ def preprocess_dblp():
 if __name__ == "__main__":
     # preprocess_copenhagen()
     # preprocess_reality_call()
+    preprocess_reality_call_v2()
     # preprocess_contacts_dublin()
     # preprocess_digg_reply()
     # preprocess_high_school_contacts()
     # preprocess_wikitalk()
     # preprocess_stackoverflow()
     # preprocess_enron()
-    preprocess_dblp()
+    # preprocess_dblp()
     
